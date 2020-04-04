4da606ce00893787162d4f0adca3d83cf1aec929
Angel Burgos <angellpublicista.1@gmail.com>
2020-04-03 22:52:57 -0500

    Updates

---
+++ precache-manifest.379dc02f983a001af938fd6fc3d8b30f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb3986dfe3126ee38a6a9f8eb9d6b45",
+    "url": "/michelsen-app/index.html"
+  },
+  {
+    "revision": "02bcd7901b07f5a7da4c",
+    "url": "/michelsen-app/static/css/main.53a2711a.chunk.css"
+  },
+  {
+    "revision": "0300aaa8fb0677dbcc5f",
+    "url": "/michelsen-app/static/js/2.beec54d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/michelsen-app/static/js/2.beec54d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bcd7901b07f5a7da4c",
+    "url": "/michelsen-app/static/js/main.4f553d2e.chunk.js"
+  },
+  {
+    "revision": "64f4c6dfa892cb8fbe78",
+    "url": "/michelsen-app/static/js/runtime-main.54f075b7.js"
+  },
+  {
+    "revision": "bbf8f6593f48b9ddc85a900533c91bc3",
+    "url": "/michelsen-app/static/media/bg-creditos.bbf8f659.jpg"
+  },
+  {
+    "revision": "249929bb5915813c8e41b2ee0b1aaae8",
+    "url": "/michelsen-app/static/media/logo-fundaciones.249929bb.svg"
+  },
+  {
+    "revision": "2c595ab0206e5ff05b21f80b77e07d59",
+    "url": "/michelsen-app/static/media/logo_blanco.2c595ab0.png"
+  }
+]);
