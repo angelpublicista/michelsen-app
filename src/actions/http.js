1e2a25ff2c03ab2815865b5b423eb75283f95a69
Angel Burgos <angellpublicista.1@gmail.com>
2020-04-03 23:32:19 -0500

    Localhost

--- src/actions/http.js
+++ src/actions/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'https://univercity.com.co/demo/michelsen/api_rest_michelsen/public/api/'
+    baseURL: 'http://localhost/api_rest_michelsen/public/api/'
 })
 
 export default http;
