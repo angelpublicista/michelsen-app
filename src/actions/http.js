aa0fb7cadef10dc0a2e78f414f46629339991c2c
Angel Burgos <angellpublicista.1@gmail.com>
2020-04-03 21:35:02 -0500

    Ruta api

--- src/actions/http.js
+++ src/actions/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost/api_rest_michelsen/public/api/'
+    baseURL: 'https://univercity.com.co/demo/michelsen/api_rest_michelsen/public/api/'
 })
 
 export default http;
